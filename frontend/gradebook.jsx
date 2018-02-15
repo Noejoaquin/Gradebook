c5644ab05c6b193ffdcc98a67eadfbe2c998afca
Noe <noe.joaquin.james@gmail.com>
2018-02-15 13:25:12 -0500

    create app and root components

--- frontend/gradebook.jsx
+++ frontend/gradebook.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logoutUser } from './util/session_api_util'
+import Root from './components/root';
 import configureStore from './store/store';
+import { login, logoutUser } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -10,5 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch = store.dispatch;
   window.login = login
   window.logoutUser = logoutUser
-  ReactDOM.render(<h1>Welcome to Gradebook</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
