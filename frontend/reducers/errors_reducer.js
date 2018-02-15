1c065231e5650e492795637296e7e36a50653fe2
Noe <noe.joaquin.james@gmail.com>
2018-02-15 13:03:27 -0500

    create error reducer

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import SessionErrorsReducer from './session_errors_reducer'
+
+const ErrorsReducer = combineReducers({
+  session: SessionErrorsReducer,
+  event: EventErrorsReducer
+});
+
+export default ErrorsReducer;
