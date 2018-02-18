428dc052c4bffeac0d5abb614d147df8220afc3c
Noe <noe.joaquin.james@gmail.com>
2018-02-18 16:34:26 -0500

    reconfigure preloaded state

--- frontend/gradebook.jsx
+++ frontend/gradebook.jsx
@@ -6,7 +6,15 @@ import { login, logout } from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  const store = configureStore();
+  // const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session : { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login
