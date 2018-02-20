4791db0783a68e0246cb53449f09d50fd9db4d21
Noe <noe.joaquin.james@gmail.com>
2018-02-20 18:30:34 -0500

    create courses reducer

---
+++ frontend/reducers/courses_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_COURSES } from '../actions/course_actions';
+import merge from 'lodash/merge';
+
+
+const CoursesReducer = (state = {}, action) => {
+  switch(action.type){
+    case RECEIVE_COURSES:
+      return action.events;
+    default:
+      return state;
+  }
+}
+
+export default EventsReducer;
