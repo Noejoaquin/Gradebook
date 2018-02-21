7e93bbb0093dbbf5df299e85d86e3620a0f4c176
Noe <noe.joaquin.james@gmail.com>
2018-02-21 11:57:45 -0500

    make course profile

---
+++ frontend/components/course/course_profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class CourseProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchCourse().then(() => this.props.fetchStudents(this.props.id))
+  }
+
+  render() {
+
+    return (
+      <div className='course-profile-container'>
+        COURSE PROFILE
+      </div>
+    );
+  }
+}
+
+export default TeacherProfile;
--- frontend/components/course/course_profile_container.jsx
+++ frontend/components/course/course_profile_container.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import CourseProfile from "./teacher_profile";
-// import { fetchCourse } from '../../actions/course_actions';
+import { fetchCourse } from '../../actions/course_actions';
 import { fetchStudents } from '../../actions/student_actions';
 
 const mapStateToProps = (state, ownProps) => {
   let id = ownProps.split('/')[2]
   let course = state.entities.courses.id
-  // let allStudents = Object.keys(state.entities.students).map((id) => state.entities.students[id])
-  // let students = allStudents.filter((student) => course.studentIds.includes(student.id))
+  let students = Object.keys(state.entities.students).map((id) => state.entities.students[id])
+
   return {
     id,
-    course
+    course,
+    students
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-
+    fetchCourse: (id) => dispatch(fetchCourse(id)),
+    fetchStudents: (data) => dispatch(fetchStudents)
   };
 };
 
