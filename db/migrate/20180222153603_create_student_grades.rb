4a643ab71b8486db8561ca398c949698bd207a83
Noe <noe.joaquin.james@gmail.com>
2018-02-22 10:39:25 -0500

    migrate create students

--- db/migrate/20180222153603_create_student_grades.rb
+++ db/migrate/20180222153603_create_student_grades.rb
@@ -1,8 +1,12 @@
 class CreateStudentGrades < ActiveRecord::Migration[5.1]
   def change
     create_table :student_grades do |t|
-
+      t.integer :course_id, null: false
+      t.integer :user_id, null: false
+      t.float :grade, null: false
       t.timestamps
     end
+    add_index :student_grades, :course_id
+    add_index :student_grades, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220193658) do
+ActiveRecord::Schema.define(version: 20180222153603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20180220193658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
+  end
+
+  create_table "student_grades", force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "user_id", null: false
+    t.float "grade", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_student_grades_on_course_id"
+    t.index ["user_id"], name: "index_student_grades_on_user_id"
   end
 
   create_table "students", force: :cascade do |t|
